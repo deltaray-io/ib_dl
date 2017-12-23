fe57d216ac7f8874885531f930f5290d3a236dc3
Tibor Kiss <tibor.kiss@gmail.com>
2017-12-23 15:58:02 +0100

    Prepare setup.py for pypi upload

--- setup.py
+++ setup.py
@@ -1,20 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 from distutils.core import setup
 
 from setuptools import find_packages
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    if os.path.exists('README.md'):
+        long_description = open('README.md').read()
+    else:
+        long_description=''
 
 install_reqs = [e.strip() for e in open("requirements.txt").readlines()]
 
 setup(
     name='ib_dl',
     version='1.0',
-    packages=find_packages(),
-    url='https://github.com/tibkiss/ib_dl',
-    license='Apache 2.0',
     author='Tibor Kiss',
     author_email='tibor.kiss@gmail.com',
     description='Historical market data downloader using Interactive Brokers TWS',
+    long_description=long_description,
+    classifiers=[
+             'Development Status :: 4 - Beta',
+             'License :: OSI Approved :: Apache Software License',
+             'Programming Language :: Python :: 3.6',
+             'Topic :: Office/Business :: Financial :: Investment'],
+    packages=find_packages(),
+    url='https://github.com/tibkiss/ib_dl',
+    license='Apache License, Version 2.0',
     install_requires=install_reqs,
     python_requires='>=3.6',
     entry_points={
